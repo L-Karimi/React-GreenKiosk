4f137e85d6dd261c01cd0ed9a9b9611c9b5d1e0d
L-Karimi <luciahkei101@gmail.com>
2022-08-25 19:38:25 +0300

    aded footer and navbar components

---
+++ src/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+const About = () => {
+  return (
+    <div>
+      <h1>This is my about page</h1>
+      <p>Our company website is build using REact</p>
+    </div>
+  );
+};
+export default About;
---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Footer from "footer";
+const Footer = () => {  
+      return (
+        <div>
+            <h1>Contacts</h1>
+            <p>Established in 2022</p>
+        </div>
+    );
+};
+export default Footer
