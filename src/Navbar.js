4eeb0cd3f4c8f12f69065df177f9b17e2c1875cd
L-Karimi <luciahkei101@gmail.com>
2022-08-25 19:43:48 +0300

    removed undeclared components

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,11 @@
 import React from "react";
-const Navbar=()=>{
-    return(
-        <div>
-                    <h1><i>Company AnitaB</i></h1>
-
-        </div>
-    );
-}
+const Navbar = () => {
+  return (
+    <div>
+      <h1>
+        <i>Company AnitaB</i>
+      </h1>
+    </div>
+  );
+};
 export default Navbar;
