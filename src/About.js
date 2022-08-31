7dd76336129d49210a93a85de023534e05ca73f1
L-Karimi <luciahkei101@gmail.com>
2022-08-31 13:28:32 +0300

    added form

--- src/About.js
+++ src/About.js
@@ -4,7 +4,30 @@ const About = () => {
   return (
     <div>
       <h1>This is my about page</h1>
-      <p>Our company website is build using REact</p>
+      <p>Our company website is build using React</p>
+
+      <div>
+        <h3>EddieShop</h3>
+        <p>Dont have an account?Sign up</p>
+        <h2>Welcome Back</h2>
+        <form>
+
+          <label for="email">Your Email</label>
+          <input type="text" id="email" name="email" placeholder="Enter your email"></input>
+          <label for="password">Password</label>
+          <input type="text" id="password" name="password" placeholder="Enter your password"></input>
+          <input type="checkbox" id="checkbox" name="checkbox"   value="logged in"></input>
+
+          <label for="checkbox">Keep me logged in</label>
+
+
+          <p>
+            Forgot password?
+          </p>
+
+        </form>
+       
+      </div>
     </div>
   );
 };
